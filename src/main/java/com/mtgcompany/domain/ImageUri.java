c25c31b76c7a1bccc3a2a957ffe8bca5205abde7
Patrick Stocklin <patrickstocklin@gmail.com>
2019-08-19 00:38:17 -0400

    added image uri pojo

---
+++ src/main/java/com/mtgcompany/domain/ImageUri.java
@@ -0,0 +1,15 @@
+package com.mtgcompany.domain;
+
+public class ImageUri {
+
+    private String small;
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+}
--- src/main/java/com/mtgcompany/domain/ScryfallResponse.java
+++ src/main/java/com/mtgcompany/domain/ScryfallResponse.java
@@ -5,6 +5,7 @@ public class ScryfallResponse {
     private String object;
     private String id;
     private Price prices;
+    private ImageUri image_uris;
 
     public String getObject() {
         return this.object;
@@ -29,4 +30,13 @@ public class ScryfallResponse {
     public void setPrice(Price price) {
         this.prices = price;
     }
+
+    public ImageUri getImage_uris() {
+        return image_uris;
+    }
+
+    public void setImage_uris(ImageUri image_uris) {
+        this.image_uris = image_uris;
+    }
+
 }
